625e46d77d11039cbc46884b2d198c425252d05c
delgadocris <37419901+delgadocris@users.noreply.github.com>
2023-03-13 16:28:30 -0400

    Add file class

---
+++ 30class.rb
@@ -0,0 +1,14 @@
+# Clase
+# Tienen caracteristicas (atributos) y comportamientos de una entidad (métodos)
+
+# Una clase es un elemento de la programación orientada a objetos que actúa como una plantilla
+# y va a definir las características y comportamientos de una entidad. La clase va a ser como
+# un molde a partir del cual vamos a poder definir entidades.
+
+# Una clase es una combinación específica de atributos y métodos y puede considerarse un tipo de dato
+# de cualquier tipo no primitivo. Así, una clase es una especie de plantilla o prototipo de objetos: define
+# los atributos que componen ese tipo de objetos y los métodos que pueden emplearse para trabajar con esos objetos.
+
+class Alumnos
+  
+end
